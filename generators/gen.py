a88c14f9d63338c64e8eff9d1229ac74ce1fcbe1
tylerLoh <tyleryin811@gmail.com>
2020-01-29 23:53:43 +0800

    add context and format code

--- generators/gen.py
+++ generators/gen.py
@@ -4,6 +4,7 @@
 # x()                       __call()__
 from time import sleep
 
+
 def add1(x, y):
     return x + y
 
@@ -15,8 +16,45 @@ class Adder:
 
 add2 = Adder()
 
+
 def compute():
-    rv = []
     for i in range(10):
         sleep(.5)
-        rv.append(i)
+        yield i
+
+
+class Compute:
+    def __iter__(self):
+        self.last = 0
+        return self
+
+    def __next__(self):
+        rv = self.last
+        self.last += 1
+        sleep(.5)
+
+        if self.last > 10:
+            raise StopIteration
+        return rv
+
+
+class Api:
+    def run_this_first(self):
+        pass
+
+    def run_this_second(self):
+        pass
+
+    def run_this_last(self):
+        pass
+
+
+# to create code that can inter live with other code and also enforce
+# it sequence
+def api():
+    first()
+    yield
+    second()
+    yield
+    last()
+
